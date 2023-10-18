d03515b679fa6e25875779201aa83e83d188d2b1
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2023-10-19 01:50:34 +0300

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,64 @@
+import { useState } from 'react';
 import { Container } from "../styles/Container";
+import images from "../constants/images";
+import { NavLink } from "react-router-dom";
+import { FiChevronDown } from 'react-icons/fi';
+import { RiMenu3Line } from 'react-icons/ri';
+import { GrClose } from 'react-icons/gr';
 
-// const HeaderEl = styled.header`
-//    /* font-size: ${(props) => props.theme.colors.size}; */
-// `;
-
+import { Buttons, ButtonsPhone, HeaderEl, HeaderWrapper, Lang, LangPhone, Logo, LogoText, Menu, MenuSmall, NavEl, Navbar, NavbarSmall } from '../styles/Home/Header';
 
 
 const Header = () => {
+   const [toggleMenu, setToggleMenu] = useState(false);
+
    return (
-      <HeaderEl>
+      <HeaderWrapper>
          <Container>
-            Header
+            <HeaderEl>
+               <Logo>
+                  <img src={images.logo} alt="logo" />
+                  <LogoText>STYLICLE</LogoText>
+               </Logo>
+               <Menu>
+                  <NavEl>
+                     <NavLink to="/" className={({ isActive }) => isActive ? 'active' : '' }>Home</NavLink>
+                     <NavLink to="/about" className={({ isActive }) => isActive ? 'active' : '' }>About Us</NavLink>
+                     <NavLink to="/contact" className={({ isActive }) => isActive ? 'active' : '' }>Contact Us</NavLink>
+                  </NavEl>
+                  <Lang>
+                     EN
+                     <FiChevronDown size={20} />
+                  </Lang>
+                  <Buttons>
+                     <button>Login</button>
+                     <button>Sign Up</button>
+                  </Buttons>
+               </Menu>
+               <Navbar>
+                  <RiMenu3Line fontSize={45} cursor={'pointer'} color={'#141414'} onClick={() => setToggleMenu(true)}/>
+                  {toggleMenu && (
+                  <NavbarSmall>
+                     <GrClose fontSize={40} cursor={'pointer'} onClick={() => setToggleMenu(false)}/>
+                     <MenuSmall>
+                        <NavLink to="/" className={({ isActive }) => isActive ? 'active' : '' }>Home</NavLink>
+                        <NavLink to="/about" className={({ isActive }) => isActive ? 'active' : '' }>About Us</NavLink>
+                        <NavLink to="/contact" className={({ isActive }) => isActive ? 'active' : '' }>Contact Us</NavLink>
+                     </MenuSmall>
+                     <LangPhone>
+                        <span className="active">En</span>
+                        <span>Ru</span>
+                     </LangPhone>
+                     <ButtonsPhone>
+                        <button>Login</button>
+                        <button>Sign Up</button>
+                     </ButtonsPhone>
+                  </NavbarSmall>
+                  )}
+               </Navbar>
+            </HeaderEl>
          </Container>
-      </HeaderEl>
+      </HeaderWrapper>
    )
 };
 
--- src/styles/Home/Header/HeaderEl.jsx
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const HeaderEl = styled.header`
-/* font-size: ${(props) => props.theme.colors.size}; */
-`;
